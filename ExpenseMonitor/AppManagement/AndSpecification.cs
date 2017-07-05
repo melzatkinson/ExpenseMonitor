ea91237335911c8d6ed7ed5a360e70d4fe68d32d
melzatkinson <melzatkinson@gmail.com>
2017-07-05 13:13:40 +0200

    Tweaked entry filtering.
    Added functionality for combining filter specs, rather than using lists of specs.

---
+++ ExpenseMonitor/AppManagement/AndSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExpenseMonitor.AppManagement
+{
+  public class AndSpecification<T> : ISpecification<T>
+  {
+    private readonly ISpecification<T> _specification1;
+    private readonly ISpecification<T> _specification2;
+
+    public AndSpecification( ISpecification<T> specification1, ISpecification<T> specification2 )
+    {
+      _specification1 = specification1;
+      _specification2 = specification2;
+    }
+
+    public bool IsSatisfied( T t )
+    {
+      return _specification1.IsSatisfied( t ) && _specification2.IsSatisfied( t );
+    }
+  }
+}
